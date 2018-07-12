9ea022546568333549e69c753b8b646bc8c0a20b
root <root@instance-2.c.iron-atom-209911.internal>
2018-07-12 19:25:03 +0000

    FIXINGTHISFUCKINGSCRIPT

--- clone_tree.sh
+++ clone_tree.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-echo -e ""Which Tree Are We Cloning Today? Nichream/TeamReloaded""
-read var1
+echo -e ""Which Tree Are We Cloning Today? nich/tre""
 
-if [ $var1 = "Nichream" ]
+read var1
+if [ $var1 = "tr" ]
   then
     git clone https://github.com/TeamReloaded/android_device_xiaomi_land -b lineage-15.1 device/xiaomi/land
     git clone https://github.com/TeamReloaded/android_kernel_xiaomi_msm8937 -b lineage-15.1 kernel/xiaomi/msm8937
     git clone https://github.com/TeamReloaded/proprietary_vendor_xiaomi -b lineage-15.1-land vendor/xiaomi
-    echo -e ""Cloned Nichream's Trees""
-    
-elif [ $var1 = "TeamReloaded" ]
+    echo "Cloned Team Reloaded's Trees"
+
+elif [ $var1 = "nich" ]
   then
     git clone https://github.com/nichcream/android_device_xiaomi_land -b lineage-15.1 device/xiaomi/land
     git clone https://github.com/nichcream/wingtech_kernel_msm8937 -b lineage-15.1 kernel/xiaomi/mam8937
     git clone https://github.com/nichcream/proprietary_vendor_xiaomi -b lineage-15.1 vendor/xiaomi
-    echo -e ""Cloned TeamReloaded's Trees""
+    echo "Cloned Nichream's Trees"
 
 fi
