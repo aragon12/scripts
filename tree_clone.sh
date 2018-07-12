6ccb04999a7a646523fb177e6e7a2690e6cc9621
root <root@instance-2.c.iron-atom-209911.internal>
2018-07-12 13:34:07 +0000

    Initial

---
+++ tree_clone.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/TeamReloaded/android_device_xiaomi_land -b lineage-15.1 device/xiaomi/land
+git clone https://github.com/TeamReloaded/android_kernel_xiaomi_msm8937 -b lineage-15.1 kernel/xiaomi/msm8937
+git clone https://github.com/TeamReloaded/proprietary_vendor_xiaomi -b lineage-15.1 vendor/xiaomi
