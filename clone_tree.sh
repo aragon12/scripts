9316d64ed065b43d7ece77a4d49178c580097fe9
Sumit Kumar <37952008+imsk17@users.noreply.github.com>
2018-07-12 23:59:36 +0530

    Fixed

--- clone_tree.sh
+++ clone_tree.sh
@@ -2,13 +2,13 @@
 echo -e ""Which Tree Are We Cloning Today? Nichream/TeamReloaded""
 read var1
 
-if [$var1 = "Nichream"]
+if [ $var1 = "Nichream" ]
   then
     git clone https://github.com/TeamReloaded/android_device_xiaomi_land -b lineage-15.1 device/xiaomi/land
     git clone https://github.com/TeamReloaded/android_kernel_xiaomi_msm8937 -b lineage-15.1 kernel/xiaomi/msm8937
     git clone https://github.com/TeamReloaded/proprietary_vendor_xiaomi -b lineage-15.1-land vendor/xiaomi
 
-elif [$var1 = "TeamReloaded"]
+elif [ $var1 = "TeamReloaded" ]
   then
     git clone https://github.com/nichcream/android_device_xiaomi_land -b lineage-15.1 device/xiaomi/land
     git clone https://github.com/nichcream/wingtech_kernel_msm8937 -b lineage-15.1 kernel/xiaomi/mam8937
