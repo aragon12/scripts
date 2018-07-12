c39a3815de843bd615d5386a14894acea3a28c4c
root <root@instance-2.c.iron-atom-209911.internal>
2018-07-12 17:27:38 +0000

    Getlost

---
+++ clone_tree.sh
@@ -0,0 +1,17 @@
+echo -e ""Which Tree Are We Cloning Today? Nichream/TeamReloaded""
+ read var1
+
+ if [var1 = "Nichream"]
+
+git clone https://github.com/TeamReloaded/android_device_xiaomi_land -b lineage-15.1 device/xiaomi/land
+git clone https://github.com/TeamReloaded/android_kernel_xiaomi_msm8937 -b lineage-15.1 kernel/xiaomi/msm8937
+git clone https://github.com/TeamReloaded/proprietary_vendor_xiaomi -b lineage-15.1-land vendor/xiaomi
+
+read var2
+if [var2 = "TeamReloaded"]
+
+git clone https://github.com/nichcream/android_device_xiaomi_land -b lineage-15.1 device/xiaomi/land
+git clone https://github.com/nichcream/wingtech_kernel_msm8937 -b lineage-15.1 kernel/xiaomi/mam8937
+git clone https://github.com/nichcream/proprietary_vendor_xiaomi -b lineage-15.1 vendor/xiaomi
+
+fi
